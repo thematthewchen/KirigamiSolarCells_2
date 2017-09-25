90dafb05a4ef447f3a4d896e713b00bde52469dc
Matthew Chen <thematthewchen@gmail.com>
2017-09-25 19:40:57 -0400

    Checks Permissions

--- app/src/main/java/com/example/kirigamisolarcells_2/MainActivity.java
+++ app/src/main/java/com/example/kirigamisolarcells_2/MainActivity.java
@@ -2,12 +2,119 @@ package com.example.kirigamisolarcells_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import java.util.Set;
+import java.util.ArrayList;
+import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart, btnEnd, btnSend, btnClear;     //Button variables for controls
+    ListView devicelist;                            //ListView variable for the list of connected devices
+    private BluetoothAdapter myBluetooth = null;    //BluetoothAdapter variable to control bluetooth
+    private Set<BluetoothDevice> pairedDevices;                      //Set variable for list of connected devices
+
+    public MainActivity(){
+        btnStart = (Button)findViewById(R.id.buttonStart);
+        btnEnd = (Button)findViewById(R.id.buttonStop);
+        btnSend = (Button)findViewById(R.id.buttonSend);
+        btnClear = (Button)findViewById(R.id.buttonClear);
+        devicelist = (ListView)findViewById(R.id.listView);
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Function allows ListView to be clicked
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener()
+    {
+        public void onItemClick (AdapterView av, View v, int arg2, long arg3)
+        {
+            // Get the device MAC address, the last 17 chars in the View
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+            // Make an intent to start next activity.
+            //Intent i = new Intent(DeviceList.this, ledControl.class);
+            //Change the activity.
+            //i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
+            //startActivity(i);
+        }
+    };
+
+    //Function for the paired Devices List
+    private void pairedDevicesList()
+    {
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+
+        if (pairedDevices.size()>0)
+        {
+            for(BluetoothDevice bt : pairedDevices)
+            {
+                list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
+            }
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        devicelist.setAdapter(adapter);
+        devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
+
+    }
+
+    //When 'Start' Button called
+    public void onClickStart(View view) {
+        if(myBluetooth == null)
+        {
+            //Show a message that the device has no bluetooth adapter
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+            //finish apk
+            finish();
+        }
+        else
+        {
+            if (myBluetooth.isEnabled())
+            { }
+            else
+            {
+                //Ask to the user turn the bluetooth on
+                Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTon,1);
+            }
+        }
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                pairedDevicesList(); //method that will be called
+            }
+        });
+    }
+
+    //When 'End' Button called
+    public void onClickEnd(View view) {
+    }
+
+    //When 'Send' Button called
+    public void onClickSend(View view) {
+    }
+
+    //When 'Clear' Button called
+    public void onClickClear(View view) {
     }
 }
